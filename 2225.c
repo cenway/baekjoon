375b9684499aebae0d8723594acae9dcfe3a4b89
hanjun yun <yhj02021212@gmail.com>
2025-11-07 00:39:25 +0900

    https://www.acmicpc.net/problem/2225

---
+++ 2225.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int arr[201][201];
+
+int main() 
+{
+	int N, k;
+	printf("asd");
+	scanf("%d %d", &N, &k);
+
+	for(int j = 0; j <= N; j++) 
+		arr[1][j] = 1;
+
+
+	for(int i = 2; i <= k; i++)
+	{
+		for(int j = 0; j <= N; j++)
+		{
+			if(j == 0) 
+			{
+				arr[i][j] = 1;
+				continue;
+			}
+			if((arr[i][j] = arr[i][j - 1] + arr[i - 1][j]) > 1000000000)
+				arr[i][j] -= 1000000000;
+
+		}
+	}
+	printf("%d", arr[k][N]);
+	return 0;
+}
