39210e20e5fcd24ba026a2011353db7e0c85e116
samsung <yhj02021212@gmail.com>
2025-12-23 16:57:18 +0900

    문자열

---
+++ 11328.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+
+int arr1[26];
+int arr2[26];
+int main() {
+	
+	int N;
+	scanf("%d", &N);
+
+	for(int i = 0; i < N; i++)
+	{
+		char tmp[1001] = {0, };
+		scanf("%s", tmp);
+		char *ptmp = tmp;
+		while(*ptmp != 0)
+		{
+			arr1[*ptmp - 'a'] += 1;
+			ptmp++;
+		}
+
+		memset(tmp, 0, sizeof(tmp));
+		scanf("%s", tmp);
+
+		ptmp = tmp;
+		while(*ptmp != 0)
+		{
+			arr2[*ptmp - 'a'] += 1;
+			ptmp++;
+		}
+
+		for(int j = 0; j < 26; j++)
+		{
+			if(arr1[j] != arr2[j])
+			{
+				printf("Impossible\n");
+				break;
+			}
+			else if(j == 25)
+				printf("Possible\n");
+		}
+		char c;
+		scanf("%c", &c);
+		memset(arr1, 0, sizeof(arr1));
+		memset(arr2, 0, sizeof(arr2));
+	}
+	return 0;
+}
+
+//문자 개수만 맞으면 가능함
+//최적화를 위해선 처음엔 +, 섞은 뒤엔 -하면 배열 하나로 가능
