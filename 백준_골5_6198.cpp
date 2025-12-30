4b7cf91e5ba300b976ca810eb763911357a8f7a4
samsung <yhj02021212@gmail.com>
2025-12-30 13:51:40 +0900

    자료형에 주의/스택

--- 백준_골5_6198.cpp
+++ 백준_골5_6198.cpp
@@ -15,7 +15,8 @@ int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	int n, tmp, ans = 0;
+	int n;
+	long long int tmp, ans = 0;
 	cin >> n;
 
 	for(int i = 0; i < n; i++)
@@ -29,7 +30,10 @@ int main() {
 			tmp = stk.top().num;
 			stk.pop();
 			if(!stk.empty())
+			{
 				stk.top().num += tmp + 1;
+			}
+				
 			tmp = 0;
 		}
 		stk.push({next, 0});
@@ -54,4 +58,5 @@ int main() {
 이럼 최악의 경우 O(n^2)
 
 top보다 큰 값이 들어오면 pop하면서 ans에 +num 뒤에는 +(num+1)
+값이 너무 커져서 long long으로 선언해야 함
 */
