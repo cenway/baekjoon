4b92611e410f411dcb770db82252a38aa3a41db9
hanjun yun <yhj02021212@gmail.com>
2026-01-04 18:48:57 +0900

    단조 스택 어렵더라

--- 백준_플5_6549.cpp
+++ 백준_플5_6549.cpp
@@ -5,27 +5,20 @@ using namespace std;
 typedef struct
 
 {
-
-	long long cnt;	int height;
-
+	long long cnt;	
+	int height;
 }p;
 
 int main() {
-
 	// 코드 작성
-
 	ios::sync_with_stdio(0);
-
 	cin.tie(0);
 
 	int n;
 
 	while(1)
-
 	{
-
 		cin >> n;
-
 		if(!n)	break;
 
 		stack<p> stk;
@@ -33,24 +26,15 @@ int main() {
 		long long max = 0;
 
 		while(n--)
-
 		{
-
 			int tmp;
-
 			long long ncnt = 1;
-
-			
 
 			cin >> tmp;
 
 			while(!stk.empty() && stk.top().height > tmp)
-
 			{
-
 				ncnt = stk.top().cnt;
-            
-
 				long long k = stk.top().height * ncnt;
 
 				if(max < k) max = k;
@@ -58,41 +42,28 @@ int main() {
 				stk.pop();
 
 				if(stk.empty() || stk.top().height < tmp) 
-
 					ncnt++;
-
 				else
                 {
                     stk.top().cnt += ncnt;
                     ncnt=1;
                 }
-
 			}
-
 			if(tmp)
-
 				stk.push({ncnt, tmp});
-
 		}
 
 		while(!stk.empty())
+		{
+			long long ncnt = stk.top().cnt;
+			long long k = stk.top().height * ncnt;
+			if(max < k) max = k;
 
-			{
+			stk.pop();
 
-				long long ncnt = stk.top().cnt;
-
-				long long k = stk.top().height * ncnt;
-
-				if(max < k) max = k;
-
-				stk.pop();
-
-				if(!stk.empty())	stk.top().cnt +=ncnt;
-
-			}
-
+			if(!stk.empty())	stk.top().cnt +=ncnt;
+		}
 		cout << max << "\n";
-
 	}
 
 	return 0;
---
+++ 백준_플5_6549_1.cpp
@@ -0,0 +1,61 @@
+/*
+1. 오버플로우 조심(계산 전에 형변환)
+2. 방향이 있다 싶으면 스택을 생각해보는데, 
+뭐를 넣어야 할지 모르겠다면 dfs
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef struct
+
+{
+	int idx;	
+	int height;
+}p;
+
+int main() {
+
+	// 코드 작성
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+
+	while(1)
+    {
+        cin >> n;
+		if(!n)	break;
+
+		stack<p> stk;
+		long long max_area = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            int tmp;
+            int nidx = i;
+			cin >> tmp;
+
+            while(!stk.empty() && stk.top().height > tmp)
+			{
+                int width = i - stk.top().idx;
+                long long k = (long long)stk.top().height*width;
+                max_area = max(max_area, k);
+
+                nidx = stk.top().idx;
+                stk.pop();
+            }
+            stk.push({nidx, tmp});
+        }
+        while(!stk.empty())
+        {
+            int width = n - stk.top().idx;
+            long long k = (long long)stk.top().height*width;
+            max_area = max(max_area, k);
+            stk.pop();
+        }
+        cout << max_area << '\n';
+    }
+    return 0;
+}
