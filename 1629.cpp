81d0aeee970ff9103eab38b2ab27a7536d0e1c5b
samsung <yhj02021212@gmail.com>
2026-01-14 17:39:23 +0900

    .

---
+++ 1629.cpp
@@ -0,0 +1,13 @@
+//모듈러 연산 ((a^n)%b)*((a^n)%b) = (a^2n)%b
+#include <iostream>
+using namespace std;
+
+int main() {
+	// 코드 작성
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	return 0;
+}
+//일단 b를 짝수로 만들고
+//a%c 
