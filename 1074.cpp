c3ace3f80527a8e4bd43cbe4119b4999653b4f1f
samsung <yhj02021212@gmail.com>
2026-01-15 17:44:25 +0900

    마저풀어

---
+++ 1074.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int r, c;
+int cnt;
+
+void func(int n, int x, int y)
+{
+	//n == 0일 때 자기 칸 확인
+	if(!n)
+	{
+		if(r == x && c == y)
+		{
+			cout << cnt;
+			exit(0);
+		}
+		cnt++;
+		return;
+	}
+	// n > 1일 때 4등분해서 시작점
+	func(n-1, x, y);
+	func(n-1, x + (1 << (n-1)), y);
+	func(n-1, x, y+ (1 << (n-1)));
+	func(n-1, x + (1 << (n-1)), y+ (1 << (n-1)));
+}
+
+int main() {
+	// 코드 작성
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	
+	int n;
+	cin >> n >> c >> r;
+	func(n, 0, 0);
+
+	return 0;
+}
