c1ffe7408547fc4d5fb4865d0765bbd1596741c5
samsung <yhj02021212@gmail.com>
2026-02-10 09:42:56 +0900

    .

--- 15683.cpp
+++ 15683.cpp
@@ -6,6 +6,7 @@ int map[8][8];
 bool visit[8][8];
 int n, m, cnt;
 vector<pair<int, int>> v[7];
+vector<pair<int, int>> vv;
 
 int dir[6][7] =
 {//x+, y+, x-, y-
@@ -33,7 +34,60 @@ int main() {
 		}
 	}
 	
+	//입력 옮겨 심기
 	for(int k = 5; k >= 1; k--)
+		for(auto i = v[k].begin(); i != v[k].end(); i++)
+			vv.push_back({i->first, i->second});
+
+	//5는 먼저 실행하고
+	//1~4를 백트래킹으로 구성
+	
+	for(auto i = v[5].begin(); i != v[5].end(); i++)
+		{
+			int x = i->first;
+			int y = i->second;
+			
+			int *ndir = &(dir[5][0]);
+			int dx[4] = {ndir[0], 0, -ndir[2], 0};
+			int dy[4] = {0, ndir[1], 0, -ndir[3]};
+			int ncnt = 0;
+			//보는 방향 고정 후 4방향에 대해 체크
+			for(int z = 0; z < 4; z++)
+			{
+				int nx = x;
+				int ny = y;
+				while(1)
+				{
+					nx += dx[z];
+					ny += dy[z];
+					if(nx<0||ny<0||nx>=n||ny>=m||map[nx][ny])
+						break;
+					if(visit[nx][ny])
+						continue;
+					ncnt++;
+					//칸 벗어나는지, 멈출 칸인지 break
+					//이미 먹힌 칸이면 continue
+					//칸 수
+				}
+			}
+			
+		}
+
+	cout << cnt;
+	return 0;
+}
+
+//일단 0들어올 때 마다 cnt++
+//높은 숫자부터 해서 5번 하자
+//visit칸을 체크하며 값이 더 높게 나오는 쪽으로
+//그냥 q나 벡터에 입력 별 좌표를 저장할까
+//거리 구하는 부분
+//방향 선택하는 부분
+//ㄴ가장 많은 칸이 선택된 인덱스를 저장 
+//visit 찍고 cnt 빼는 부분
+
+
+for(int k = 5; k >= 1; k--)
 	{
 		for(auto i = v[k].begin(); i != v[k].end(); i++)
 		{
@@ -80,17 +134,3 @@ int main() {
 			
 		}
 	}
-
-	cout << cnt;
-	return 0;
-}
-
-//일단 0들어올 때 마다 cnt++
-//높은 숫자부터 해서 5번 하자
-//visit칸을 체크하며 값이 더 높게 나오는 쪽으로
-//그냥 q나 벡터에 입력 별 좌표를 저장할까
-//4개를 깡으로 구현해야 돼? 진짜?
-//거리 구하는 부분
-//방향 선택하는 부분
-//ㄴ가장 많은 칸이 선택된 인덱스를 저장 
-//visit 찍고 cnt 빼는 부분
